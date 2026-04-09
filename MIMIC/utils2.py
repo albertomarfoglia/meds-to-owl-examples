58a08bae3a5f5773451504d267c5dc3f55f41637
Alberto Marfoglia <a.marfoglia@hotmail.it>
2026-04-09 12:18:44 +0000

    feature: add MIMIC tasks

--- MIMIC/utils2.py
+++ MIMIC/utils2.py
@@ -1,12 +1,15 @@
 import polars as pl
 
 
-def generate_samples(n_folds: int, labels: pl.LazyFrame, size: int, seed: int | None):
+def generate_samples(n_folds: int, labels: pl.LazyFrame, size: int, seed: int | None, fixed_true = False):
     true_values = (
         labels.filter(pl.col("boolean_value"))
-        .sort(pl.col("prediction_time"), descending=True)
+        # .sort(pl.col("prediction_time"), descending=False)
+        # .group_by("subject_id")
+        # .head(1)
         .group_by("subject_id")
-        .head(1)
+        .agg(pl.all().sample(n=1, seed=seed))
+        .explode(pl.all().exclude("subject_id"))
         .collect(engine="streaming")
     )
 
@@ -17,25 +20,33 @@ def generate_samples(n_folds: int, labels: pl.LazyFrame, size: int, seed: int | 
             on="subject_id",
             how="anti",
         )
+        # .sort(pl.col("prediction_time"), descending=False)
+        # .group_by("subject_id")
+        # .head(1)
         .group_by("subject_id")
-        .head(1)
+        .agg(pl.all().sample(n=1, seed=seed))
+        .explode(pl.all().exclude("subject_id"))
         .collect(engine="streaming")
     )
 
     fold_length = int(size / 2)
 
-    true_values = true_values.sample(n=fold_length * n_folds, shuffle=True, seed=seed)
-    false_values = false_values.sample(n=fold_length * n_folds, shuffle=True, seed=seed)
-
-    true_samples = [
-        true_values.slice(i * fold_length, fold_length) for i in range(n_folds)
-    ]
-
+    false_values = false_values.sample(n=fold_length * n_folds, shuffle=True, seed=seed, with_replacement=False)
     false_samples = [
         false_values.slice(i * fold_length, fold_length) for i in range(n_folds)
     ]
 
-    return [false_samples[i].extend(true_samples[i]) for i in range(n_folds)]
+    if fixed_true:
+        true_values = true_values.sample(n=fold_length, shuffle=True, seed=seed, with_replacement=False)
+        return [false_samples[i].extend(true_values) for i in range(n_folds)]
+    else:
+        true_values = true_values.sample(n=fold_length * n_folds, shuffle=True, seed=seed, with_replacement=False)
+
+        true_samples = [
+            true_values.slice(i * fold_length, fold_length) for i in range(n_folds)
+        ]
+
+        return [false_samples[i].extend(true_samples[i]) for i in range(n_folds)]
 
 
 bad_strings = ["___", "None", "N/A", "", " "]
@@ -50,14 +61,27 @@ remove_long_text = (
     .alias("text_value")
 )
 
+birthdate_to_age = [
+    (
+        pl.when(pl.col("code") == "MEDS_BIRTH")
+        .then((pl.col("prediction_time") - pl.col("time")) / pl.duration(days=365))
+        .otherwise(None)
+        .alias("numeric_value")
+    ),
+    pl.when(pl.col("code") == "MEDS_BIRTH")
+    .then(None)
+    .otherwise(pl.col("time"))
+    .alias("time"),
+]
+
 before_prediction_time = (pl.col("time").is_null()) | (
     pl.col("time") <= pl.col("prediction_time")
 )
 
-swap_text_with_numeric = (
-    pl.coalesce(pl.col("parsed"), pl.col("numeric_value")),
-    pl.when(pl.col("parsed").is_not_null()).then(None).otherwise(pl.col("text_value")),
-)
+swap_text_with_numeric = ({
+    "numeric_value": pl.coalesce(pl.col("parsed"), pl.col("numeric_value")),
+    "text_value": pl.when(pl.col("parsed").is_not_null()).then(None).otherwise(pl.col("text_value")),
+})
 
 parse_txt_to_float = pl.col("text_value").cast(pl.Float64, strict=False)
 
@@ -119,8 +143,7 @@ derived_columns = ["bp_values", "orig_code", "orig_text", "parsed"]
 
 
 def extract_labels_array(outcomes: pl.DataFrame):
-    return (
-        outcomes.select(["subject_id", "boolean_value"])
+    labels = (outcomes.select(["subject_id", "boolean_value"])
         .group_by("subject_id")
         .agg(pl.col("boolean_value").first().cast(pl.Int8).alias("boolean_value"))
         .sort("subject_id")
@@ -128,3 +151,4 @@ def extract_labels_array(outcomes: pl.DataFrame):
         .to_series()
         .to_list()
     )
+    return labels
